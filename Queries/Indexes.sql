2e667114d92ef7229ec98c20a9a85ee926e3f305
achaika01 <achaika@kse.org.ua>
2025-06-18 15:45:29 +0300

    Update Indexes.sql

--- Queries/Indexes.sql
+++ Queries/Indexes.sql
@@ -1,5 +1,3 @@
-ALTER TABLE animals ADD FULLTEXT INDEX `fulltext`(scientific_name);
-
 ALTER TABLE investors ADD FULLTEXT INDEX `fulltext`(investor_name, investor_surname);
 
 ALTER TABLE staff ADD FULLTEXT INDEX `fulltext`(name, surname);
